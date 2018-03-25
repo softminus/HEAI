b86a44c5a8a6da1594c00384391f1a8969f73771
Kia <2592730+superbaud@users.noreply.github.com>
2018-03-25 16:52:53 -0700

    0.5 offset

--- qwave_romgen.py
+++ qwave_romgen.py
@@ -7,7 +7,7 @@ scale = 15
 
 
 for i in range(0,samples):
-    x = math.pi*(i+2)/(2*samples)
+    x = math.pi*(i+0.5)/(2*samples)
     f = "{:04x}"
     val = int(math.sin(x)*scale)
     print(f.format(val))
