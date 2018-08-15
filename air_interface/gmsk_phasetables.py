45780f89e502b5e4955456b77953e83e9709f9fb
Kia <2592730+superbaud@users.noreply.github.com>
2018-08-14 21:37:59 -0700

    starting to look decent.

--- air_interface/gmsk_phasetables.py
+++ air_interface/gmsk_phasetables.py
@@ -5,17 +5,20 @@ from math import *
 
 samples = 1024
 
-delta = sqrt(2/log(2)) * pi * 0.337
+beta = pi * 0.3 * sqrt(2 / log(2))
 
 def erfint(normalised_time): # normalised time goes from 0 to 1 to represent 0 to T_b
-    erfi = pi/2 * normalised_time * erf( delta * normalised_time ) + (1/2) * (sqrt(pi) / delta) * exp(-(normalised_time**2))
+    erfi = normalised_time * erf(beta * normalised_time) + (1 / ( beta *sqrt(pi))) * exp(-((beta*normalised_time)**2))
     return erfi
 
 def traj_three(t):
-    return erfint(t) - erfint(0.0)
+    return 0.5 * pi * (erfint(t) - erfint(0.0))
 
 def traj_two(t):
-    return erfint(t) - erfint(0.0) - erfint(t-1) + 1 - t
+    return 0.5 * pi * (erfint(t) - erfint(0.0) - erfint(t-1) + 1 - t)
+
+def traj_seven(t):
+    return 0.5 * pi * t
 
 for i in range(0,samples+1):  # from zero (inclusive) to 64 (EXCLUSIVE)
     time = i/samples        # time from 0 to 1 (to represent real time 0 to T_b)
