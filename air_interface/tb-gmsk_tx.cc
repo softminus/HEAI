2f068438cdd289e0895133c66449606c95b0e467
Kia <2592730+superbaud@users.noreply.github.com>
2018-08-23 16:30:57 -0700

    sample multiple times per clock interval, to capture combinatorial logic settling

--- air_interface/tb-gmsk_tx.cc
+++ air_interface/tb-gmsk_tx.cc
@@ -7,7 +7,7 @@
 #include <math.h>
 
 int main(int argc, char **argv, char **env) {
-int i;
+    int tick_count;
     int clk;
     Verilated::commandArgs(argc, argv);
     // init gmsk_tx verilog instance
@@ -18,27 +18,41 @@ int i;
     gmsk_tx->trace (tfp, 99);
     tfp->open ("gmsk_tx.vcd");
     // initialize simulation inputs
-    gmsk_tx->clock = 1;
+    gmsk_tx->clock = 0;
     // run simulation for 1000 crystal periods
-    for (i=0; i<1000; i++) {
+    for (tick_count=1; tick_count<1000; tick_count++) {
 //            gmsk_tx->rf_in_i = (sin(2*i/100.0) * 128.0);
 //            gmsk_tx->rf_in_q = (cos(2*i/100.0) * 128.0);
 //            gmsk_tx->lo_in_i = (sin(2.5*i/100.0) * 128.0);
 //            gmsk_tx->lo_in_q = -(cos(2.5* i/100.0) * 128.0);
 
+        gmsk_tx->clock = 0;
+        gmsk_tx->eval ();
+        tfp->dump (10*tick_count-2);
 
-        // dump variables into VCD file and toggle crystal
-        for (clk=0; clk<2; clk++) {
-            tfp->dump (2*i+clk);
-            gmsk_tx->clock = !gmsk_tx->clock;
-            if(i == 30 && clk ==1) {
-                gmsk_tx->symbol_strobe = 1;
-            } else if (i == 31 && clk==0) {
-                gmsk_tx->symbol_strobe=0;
-            }
-            printf("%d %d\n", 2*i+clk, gmsk_tx->inphase_out);
-            gmsk_tx->eval ();
+        gmsk_tx->clock = 1;
+        gmsk_tx->eval ();
+        tfp->dump (10*tick_count);
+
+        if (tick_count%7==0) {
+            gmsk_tx->sample_strobe = 1;
+        } else {
+            gmsk_tx->sample_strobe = 0;
         }
+
+        if (tick_count%23==0) {
+            gmsk_tx->symbol_strobe = 1;
+        } else {
+            gmsk_tx->symbol_strobe = 0;
+        }
+
+
+        gmsk_tx->clock = 0;
+        gmsk_tx->eval ();
+        tfp->dump (10*tick_count+5);
+        tfp->flush();
+
+        printf("%d %d\n", tick_count, gmsk_tx->inphase_out);
         if (Verilated::gotFinish())    exit(0);
     }
     tfp->close();
