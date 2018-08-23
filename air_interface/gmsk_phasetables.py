fc348c1e555c666c72c145fc2077c94152fff410
Kia <2592730+superbaud@users.noreply.github.com>
2018-08-23 10:53:14 -0700

    output non-hex of all four

--- air_interface/gmsk_phasetables.py
+++ air_interface/gmsk_phasetables.py
@@ -32,6 +32,9 @@ curve_table_3 = open("gmsk_curve_3.hex",'w')
 curve_table_7 = open("gmsk_curve_7.hex",'w')
 
 curve_table_1_plain = open("gmsk_curve_1.txt",'w')
+curve_table_2_plain = open("gmsk_curve_2.txt",'w')
+curve_table_3_plain = open("gmsk_curve_3.txt",'w')
+curve_table_7_plain = open("gmsk_curve_7.txt",'w')
 
 
 
@@ -77,9 +80,13 @@ for i in range(0,samples):  # from zero (inclusive) to 64 (EXCLUSIVE)
     time = i/samples        # time from 0 to 1 (to represent real time 0 to T_b)
     f = "{:04x}"
     print (f.format(master_curve_one(time))   , file=curve_table_1)
-    print (i,       master_curve_one(time)    , file=curve_table_1_plain)
     print (f.format(master_curve_two(time))   , file=curve_table_2)
     print (f.format(master_curve_three(time)) , file=curve_table_3)
     print (f.format(master_curve_seven(time)) , file=curve_table_7)
+
+    print (i,       master_curve_one(time)    , file=curve_table_1_plain)
+    print (i,       master_curve_two(time)    , file=curve_table_2_plain)
+    print (i,       master_curve_three(time)  , file=curve_table_3_plain)
+    print (i,       master_curve_seven(time)  , file=curve_table_7_plain)
 
     
