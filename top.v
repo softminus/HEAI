06dc1ae55792967fb81a64b91b53d2b8d40d630f
Kia <2592730+superbaud@users.noreply.github.com>
2018-11-04 20:52:14 -0800

    internal strobe for top.v

--- top.v
+++ top.v
@@ -1,13 +1,13 @@
 `default_nettype none
-module top (crystal, sy_st, sa_st, in_bits, out_i, out_q);
+module top (crystal, in_bits, out_i, out_q);
     input crystal;
 
-    input wire sy_st, sa_st, in_bits;
+    input wire in_bits;
 
     output reg [7:0] out_i;
     output reg [7:0] out_q;
     
-    gmsk_tx tx(crystal, sy_st, sa_st, in_bits, out_i, out_q);
+    gmsk_tx tx(crystal, in_bits, out_i, out_q);
 
 endmodule
 
