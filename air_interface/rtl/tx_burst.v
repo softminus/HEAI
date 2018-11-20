3e3d7c5f5efa6c61c47a5eab3ffbe0c11e5ef8f4
Kia <2592730+superbaud@users.noreply.github.com>
2018-11-19 20:59:36 -0800

    think i have iq strobe pinchoff working

--- air_interface/rtl/tx_burst.v
+++ air_interface/rtl/tx_burst.v
@@ -9,8 +9,9 @@ module tx_burst
     input wire clock,
 
     // timing
-    input wire next_symbol_strobe, // modulator asserts this every symbol-interval,
-    output reg current_symbol,     // ...so we can feed symbols to the modulator
+    input wire symbol_input_strobe, // high when the modulator expects a new symbol
+    input wire symbol_iq_strobe,    // high on the first I/Q samples of the next symbol
+    output reg current_symbol,      // value of symbol to emit
 
     output wire sample_strobe,      // we assert this every sample-interval
 
@@ -36,11 +37,14 @@ module tx_burst
 );
 
     localparam ROM_OUTPUT_BITS = 7;
-    localparam CLOCKS_PER_SAMPLE = 13;
+    localparam CLOCKS_PER_SAMPLE = 5;
 
+    reg [(ROM_OUTPUT_BITS-1+1):0] pipeline_inphase;
+    reg [(ROM_OUTPUT_BITS-1+1):0] pipeline_quadrature;
     reg reset;
     reg [3:0] priming;
     reg lockout;
+    reg primed;
 
     reg [(CLOCKS_PER_SAMPLE-1):0] clkdiv;
 
@@ -51,27 +55,34 @@ module tx_burst
             clkdiv  <= 1;
         end else begin
             clkdiv <= {clkdiv[(CLOCKS_PER_SAMPLE-2):0], clkdiv[(CLOCKS_PER_SAMPLE-1)]};
-            sample_strobe <= clkdiv[0];
+            sample_strobe <= 1; //clkdiv[0];
         end // end else
 
         if (priming != 0) begin
             current_symbol <= 1;
-            if ((lockout == 0) && (next_symbol_strobe == 1)) begin
+            if ((lockout == 0) && (symbol_input_strobe == 1)) begin
                 lockout <= 1;
                 priming <= {1'b0, priming[3:1]};
-            end // if (next_symbol_strobe == 1)
-            if (next_symbol_strobe == 0) begin
+            end // if (symbol_input_strobe == 1)
+            if (symbol_input_strobe == 0) begin
                 lockout <= 0;
-            end // if ((lockout == 1) && (next_symbol_strobe == 0))
+            end // if ((lockout == 1) && (symbol_input_strobe == 0))
         end // if (priming != 0)
 
-        if (priming != 0) begin
-            rfchain_inphase <= 0;
-            rfchain_quadrature <= 0;
+        if ((priming == 0) && (primed == 0)) begin
+            if (symbol_iq_strobe == 1) begin
+                primed <= 1;
+            end // if (symbol_iq_strobe == 1)
+        end // if ((priming == 0) && (primed == 0))
+        pipeline_inphase <= modulator_inphase;
+        pipeline_quadrature <= modulator_quadrature;
+        if (primed == 0) begin
+            rfchain_inphase <= 1;
+            rfchain_quadrature <= 1;
             iq_valid <= 0;
         end else begin
-            rfchain_inphase    <= modulator_inphase;
-            rfchain_quadrature <= modulator_quadrature;
+            rfchain_inphase    <= pipeline_inphase;
+            rfchain_quadrature <= pipeline_quadrature;
             iq_valid <= 1;
             current_symbol <= 0;
         end // end else
