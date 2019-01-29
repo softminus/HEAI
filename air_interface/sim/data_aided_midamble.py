81195601754ae39daa52799d937589a2d9b1f6a1
Kia <2592730+superbaud@users.noreply.github.com>
2019-01-28 17:04:09 -0800

    correlation with pseudo-midamble

--- air_interface/sim/data_aided_midamble.py
+++ air_interface/sim/data_aided_midamble.py
@@ -18,7 +18,7 @@ def gnuplotize(data):
 if (len(sys.argv) == 2):
     data_dir = sys.argv[1]
 else:
-    data_dir = os.getcwd()
+    data_dir = "../.."
 
 path = "verilog_out.txt"
 f = open(os.path.join(data_dir,path),'r')
@@ -32,7 +32,7 @@ x.pop() #get rid of newline
 i_samples = list(map(lambda x:(int(x[1])-255), x))
 q_samples = list(map(lambda x:(int(x[2])-255), x))
 
-samples = list(map(lambda x,y: x+y*1j, i_samples, q_samples))
+samples = list(map(lambda x,y: x/255.0+y/255.0*1j, i_samples, q_samples))
 
 print(samples)
 
@@ -41,3 +41,26 @@ energy = list(map(lambda x: abs(x), samples))
 
 gnuplotize(energy)
 
+def conj(x):
+    z = x.real - x.imag * 1j
+    return z
+
+def corr_amp(needle, haystack):
+    assert(len(needle) == len(haystack))
+    return abs(sum(list(map(lambda x, y: x * conj(y), needle, haystack))))
+
+hajime = 641
+
+
+def shift_eye(seq, offset):
+    return samples[(hajime + 30)*samples_per_symbol + offset: (hajime+50)*samples_per_symbol +offset]
+
+pseudomidamble = shift_eye(samples, 0)
+
+print(len(pseudomidamble))
+
+corrs = []
+for i in range(-4000,4000):
+    corrs.append(corr_amp(pseudomidamble,shift_eye(samples,i)))
+
+gnuplotize(corrs)
