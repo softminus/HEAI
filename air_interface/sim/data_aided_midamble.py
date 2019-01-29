8bb6854f5be625c0c7d67c216a2570925d7b4439
Kia <2592730+superbaud@users.noreply.github.com>
2019-01-28 16:21:43 -0800

    use cmath

--- air_interface/sim/data_aided_midamble.py
+++ air_interface/sim/data_aided_midamble.py
@@ -6,10 +6,13 @@ import os
 import sys
 import random
 
+
+samples_per_symbol = 64
+
 def gnuplotize(data):
     outf = open(os.path.join(data_dir, "python_out.txt"), 'w')
     for idx, val in enumerate(data):
-        f = "{:d} {:d}"
+        f = "{:d} {:f}"
         print (f.format(idx, val), file=outf)
 
 if (len(sys.argv) == 2):
@@ -26,14 +29,15 @@ x = list(map((lambda x:str.split(x,' ')),str.split(lines,'\n')))
 x.pop() #get rid of newline
 
 
-i_samples = list(map(lambda x:int(x[1]), x))
-q_samples = list(map(lambda x:int(x[2]), x))
+i_samples = list(map(lambda x:(int(x[1])-255), x))
+q_samples = list(map(lambda x:(int(x[2])-255), x))
 
-samples = list(zip(i_samples,q_samples))
+samples = list(map(lambda x,y: x+y*1j, i_samples, q_samples))
 
 print(samples)
 
-energy = list(map(lambda x: (x[0]-255)**2 + (x[1]-255)**2, samples))
+energy = list(map(lambda x: abs(x), samples))
 
 
 gnuplotize(energy)
+
