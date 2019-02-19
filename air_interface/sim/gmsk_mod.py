a664e01f5df5b743978ad140f86ae970266dc9fb
Kia <2592730+superbaud@users.noreply.github.com>
2019-02-18 20:44:10 -0800

    we'll have a phase trellis...any second now

--- air_interface/sim/gmsk_mod.py
+++ air_interface/sim/gmsk_mod.py
@@ -15,6 +15,8 @@ import numpy as np
 
 samples_per_symbol = 16
 
+
+
 def gnuplotize(data):
     outf = open(os.path.join(data_dir, "python_out.txt"), 'w')
     for idx, val in enumerate(data):
@@ -27,13 +29,38 @@ else:
     data_dir = "../.."
 
 
+bt = 0.3
+sigma = sqrt(np.log(2) / (4 * np.pi * np.pi * (bt*bt)))
+bigT = 1
+
 
 def qfun(x):
     return (0.5 * erfc(x/(sqrt(2))))
 
-x = np.linspace(-10,10)
+def frequency_shaping_pulse(x):
+    scale = 1/(2*bigT)
+    firstQ  = qfun((x/bigT - 0.5)/sigma)
+    secondQ = qfun((x/bigT + 0.5)/sigma)
+    return (scale * (firstQ - secondQ))
 
-z = np.vectorize(qfun)(x)
+
+def bigPhi(x):
+    return (1.0 - qfun(x))
+
+def bigG(x):
+    first_half  = x * bigPhi(x/sigma)
+    second_half = (sigma / sqrt(2*np.pi)) * np.exp(-(x*x)/(2*sigma*sigma))
+    return first_half+second_half
+
+def phase_shaping_pulse(x):
+    foo = bigG((x / bigT) + 0.5) - bigG((x / bigT) - 0.5)
+    return 0.5 * foo
+
+def multisymbols(x):
+    return phase_shaping_pulse(x) - phase_shaping_pulse(x-5) + phase_shaping_pulse(x-1)
+
+x = np.linspace(-20,20,num=20*100)
+z = np.vectorize(multisymbols)(x)
 
 
 plt.plot(x,z)
