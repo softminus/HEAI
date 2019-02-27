40225e1f4814e44cf9508bb96a155f7f77637443
Kia <2592730+superbaud@users.noreply.github.com>
2019-02-27 11:01:00 -0800

    working

--- gmsk_sim/simrunner.py
+++ gmsk_sim/simrunner.py
@@ -24,20 +24,12 @@ range_echoez = np.linspace(0,164,num=164*samples_per_symbol)
 gmsk_modu = gmsk_modulator_warmup(samples_per_symbol)
 
 print(len(rangez))
+
 for i in range(0,1):
     symbol_array = np.random.randint(0,2,150) * 2 - 1
     hadaka = np.zeros_like(rangez)
     z = gmsk_modulate(symbol_array, samples_per_symbol, gmsk_modu)
-    plt.plot(np.real(z), np.imag(z)
-# 
-   munged_signal = cost(signal)
-    #plt.plot(np.real(munged_signal),np.imag(munged_signal))
-    #plt.show()
-
-#    plt.plot(range_echoez, np.real(munged_signal)+6)
-#    plt.plot(range_echoez, np.imag(munged_signal)+9)
-
-
-#    plt.plot(rangez, qsam)
+    plt.plot(np.imag(z))
+    plt.plot(np.real(z))
 
 plt.show()
