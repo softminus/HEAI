73c6434214e0dc6de18eb9cee6eb14be4746184c
Kia <2592730+superbaud@users.noreply.github.com>
2019-03-07 16:50:59 -0800

    new channel simulation

--- gmsk_sim/simrunner.py
+++ gmsk_sim/simrunner.py
@@ -24,7 +24,8 @@ range_echoez = np.linspace(0,164,num=164*samples_per_symbol)
 gmsk_modu = modulator.warmup(samples_per_symbol)
 
 print(len(rangez))
-
+cir = chansim.channel_ir(chansim.HT_1, samples_per_symbol)
+print(cir)
 for i in range(0,1):
     symbol_array = np.random.randint(0,2,150) * 2 - 1
     hadaka = np.zeros_like(rangez)
@@ -32,7 +33,7 @@ for i in range(0,1):
     #plt.plot(np.real(z)+6)
     #plt.plot(np.imag(z)+6)
 
-    z = chansim.cost(z, samples_per_symbol)
+    z = chansim.channel_sim(z, cir, samples_per_symbol)
     plt.plot(np.real(z), np.imag(z))
     #plt.plot(np.imag(z))
 
