521b2842f3e819446ce1e0315230a95f4b5261dc
Kia <2592730+superbaud@users.noreply.github.com>
2019-03-07 16:51:11 -0800

    start work on burst assembly

---
+++ gmsk_sim/gmsk/burstgen.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+
+# burst assembly
+
+# 3 tail
+# 57 data
+# 1 control
+
+# 26 midamble
+
+# 1 control
+# 57 data
+# 3 tail
+
+
+# THEN DIFFENCODE and send to {1,-1}
+
+
+
+def normal_burst()
