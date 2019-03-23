806996d54750e43bdcdc97e9d92bd8d714de4d73
Kia <2592730+superbaud@users.noreply.github.com>
2019-03-23 00:59:15 -0700

    awgn + decimate

--- rx_sim/simrunner.py
+++ rx_sim/simrunner.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
 
-from gmsk import modulator, chansim
+from gmsk import modulator, chansim, utility
 import os
 import sys
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy.signal as sp
 
 
 
@@ -27,13 +28,16 @@ print(len(rangez))
 cir = chansim.channel_ir(chansim.BU_1, samples_per_symbol)
 print(cir)
 for i in range(0,1):
-    symbol_array = np.random.randint(0,1,150) * 2 - 1
+    symbol_array = utility.prbs(150)
     hadaka = np.zeros_like(rangez)
     sig = modulator.modulate(symbol_array, samples_per_symbol, gmsk_modu)
     #plt.plot(np.real(z)+6)
     #plt.plot(np.imag(z)+6)
 
-    z = chansim.channel_sim(sig, cir, samples_per_symbol)
+    #z = chansim.channel_sim(sig, cir, samples_per_symbol)
+    z = chansim.awgn(sig, 18, 2, samples_per_symbol=samples_per_symbol)
+    z = sp.decimate(z,16)
+    sig = sp.decimate(sig,16)
     plt.plot(np.real(z), np.imag(z))
     plt.plot(np.real(sig)+0, np.imag(sig)+3)
     plt.show()
