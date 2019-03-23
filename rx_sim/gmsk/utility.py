c314983a89a32bed04ea080c770912f0e28bc15e
Kia <2592730+superbaud@users.noreply.github.com>
2019-03-23 00:58:43 -0700

    prbs

--- rx_sim/gmsk/utility.py
+++ rx_sim/gmsk/utility.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
+import numpy as np
+
+
 def gnuplotize(data):
     outf = open(os.path.join(data_dir, "python_out.txt"), 'w')
     for idx, val in enumerate(data):
         f = "{:f} {:f}"
         print (f.format(idx/samples_per_symbol, val), file=outf)
+
+def prbs(length):
+    return (np.random.randint(0,2,length) * 2 - 1)
