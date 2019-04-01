88a20d7d39ded2610df4e2088e4da817043aba5a
Kia <2592730+superbaud@users.noreply.github.com>
2019-03-31 19:05:16 -0700

    mv

--- rx_sim/data_aided_midamble.py
+++ rx_sim/data_aided_midamble.py
@@ -8,38 +8,9 @@ import os
 import sys
 import random
 
-
-samples_per_symbol = 64
-
-def gnuplotize(data):
-    outf = open(os.path.join(data_dir, "python_out.txt"), 'w')
-    for idx, val in enumerate(data):
-        f = "{:f} {:f}"
-        print (f.format(idx/samples_per_symbol, val), file=outf)
-
-if (len(sys.argv) == 2):
-    data_dir = sys.argv[1]
-else:
-    data_dir = "../.."
-
-path = "verilog_out.txt"
-f = open(os.path.join(data_dir,path),'r')
-
-lines = f.read()
-x = list(map((lambda x:str.split(x,' ')),str.split(lines,'\n')))
-x.pop() #get rid of newline
-
-
-i_samples = list(map(lambda x:(int(x[1])-255), x))
-q_samples = list(map(lambda x:(int(x[2])-255), x))
-
-samples = list(map(lambda x,y: x/255.0+y/255.0*1j, i_samples, q_samples))
-
-
 energy = list(map(lambda x: abs(x), samples))
 
 
-#gnuplotize(energy)
 
 def conj(x):
     z = x.real - x.imag * 1j
